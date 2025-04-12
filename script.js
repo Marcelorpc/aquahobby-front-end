d3773424cce0164e47725a6b12e2ad9cdd4491f4
MarceloRPC <marcelorpchaves@gmail.com>
2025-04-12 10:27:05 -0300

    implement rendering of aquarium list from API

---
+++ script.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", async () => {
+  try {
+    const aquariums = await fetchAquariums();
+    console.log(aquariums)
+    renderAquariumTable(aquariums);
+  } catch (error) {
+    console.error("Erro ao carregar aquários:", error);
+    document.querySelector("tbody").innerHTML = "<tr><td colspan='5'>Erro ao carregar dados.</td></tr>";
+  }
+});
+
+// Função para buscar a lista de quarios da API
+async function fetchAquariums() {
+  try {
+    const response = await fetch('http://localhost:5000/aquarios');
+    
+    if (!response.ok) {
+      throw new Error(`Erro na requisição: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data.aquarios;
+  } catch (error) {
+    console.error('Erro:', error.message);
+  }
+}
+
+// Função para montar a tabela
+function renderAquariumTable(aquariums) {
+  const tableBody = document.querySelector("tbody");
+  tableBody.innerHTML = "";
+
+  aquariums.forEach(aquario => {
+    const tr = document.createElement("tr");
+
+    tr.innerHTML = `
+      <td>${aquario.nome}</td>
+      <td>${aquario.volume}L</td>
+      <td>${aquario.temperatura}°</td>
+      <td>${aquario.ph}</td>
+      <td>
+        <button onclick="editarAquario(${aquario.id})">Editar</button>
+        <button onclick="removerAquario(${aquario.id})">Remover</button>
+      </td>
+    `;
+
+    tableBody.appendChild(tr);
+  });
+}
